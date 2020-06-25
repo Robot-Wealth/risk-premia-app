9649cb0497819dff05101dc7d1f82b3459fa716f
robotmaterkris <kris@robotwealth.com>
2020-06-25 11:02:53 +1000

    ui updates

--- risk-premia-app/app.R
+++ risk-premia-app/app.R
@@ -2,15 +2,16 @@ library(shiny)
 library(tidyverse)
 library(here)
 library(shinyjs)
+library(shinyWidgets)
 
 theme_set(theme_bw())
 
-# load data
-# load(here::here("risk-premia-app", "data", "assetclass_prices.RData"))  # to run locally
-load("data/assetclass_prices.RData")  # for deployment
+# load data, functions
+load(here::here("risk-premia-app", "data", "assetclass_prices.RData"))  # to run locally
+source(here::here("risk-premia-app", "R", "utils.R")) # to run locally
 
-# source(here::here("risk-premia-app", "R", "utils.R")) # to run locally
-source("R/utils.R")  # for deployment
+# source("R/utils.R")  # for deployment
+# load("data/assetclass_prices.RData")  # for deployment
 
 # TODO:
     # load from BQ: do we want the unwashed masses hitting our cloud?
@@ -22,6 +23,7 @@ source("R/utils.R")  # for deployment
     # tour
     # rolling corrplot - should use different colours as they don't represent the things above....
     # numbers over tiles on corrplot
+    # tooltip for freq to capitalise profits
     
 
 
@@ -33,58 +35,111 @@ prices <- prices %>%
     filter(ticker %in% rp_tickers) %>% 
     add_total_returns_col()
 
-ui <- fluidPage(
+ui <- navbarPage(
     shinyjs::useShinyjs(),
     
-    titlePanel("Risk Premia Harvesting"),
+    title = "Risk Premia Harvesting",
+    id = "tab",
+    selected = "perfTab",
     
-    sidebarLayout(
-        sidebarPanel(
-            selectizeInput("assets", "Select Assets", choices = c("VTI", "TLT", "GLD"), selected = c("VTI", "TLT", "GLD"), multiple = TRUE),
-            sliderInput("estWdwSize", "Select Estimation Window Length", min = 10, max = 100, step = 10, value = 30),
-            sliderInput("fwdWdwSize", "Select Forward Window Length", min = 10, max = 100, step = 10, value = 30),
-            checkboxInput("removeOverlapping", label = "Show Non-Overlapping Periods Only", value = TRUE)
-        ),
+    tabPanel(
+        "Performance",
+        value = "perfTab",
+    
+        sidebarLayout(
+            sidebarPanel(
+                selectizeInput("assets", "Select Assets", choices = c("VTI", "TLT", "GLD"), selected = c("VTI", "TLT", "GLD"), multiple = TRUE),
+            ),
         
-        mainPanel(
-            tabsetPanel(
-                id = "tab",
-                tabPanel(
-                    "Performance",
-                    value = "perfTab",
-                    plotOutput("cumReturnsPlot"),
-                    plotOutput("rollingPerfPlot"),
-                    plotOutput("rollCorrPlot"),
-                    plotOutput("corrMatPlot")
-                ),
+            mainPanel(
+                plotOutput("cumReturnsPlot"),
+                plotOutput("rollingPerfPlot"),
+                plotOutput("rollCorrPlot"),
+                plotOutput("corrMatPlot")
+            )
+        )
+    ),
             
-                tabPanel(
-                    "Scatterplots",
-                    value = "autcorrTab",
-                    fluidRow(
-                        column(6, plotOutput("laggedReturnsPlot")),
-                        column(6, plotOutput("laggedVolPlot"))
-                    )
+    tabPanel(
+        "Scatterplots",
+        value = "autcorrTab",
+        sidebarLayout(
+            sidebarPanel(
+                selectizeInput("assets", "Select Assets", choices = c("VTI", "TLT", "GLD"), selected = c("VTI", "TLT", "GLD"), multiple = TRUE),
+                sliderInput("estWdwSize", "Select Estimation Window Length", min = 10, max = 100, step = 10, value = 30),
+                sliderInput("fwdWdwSize", "Select Forward Window Length", min = 10, max = 100, step = 10, value = 30),
+                checkboxInput("removeOverlapping", label = "Show Non-Overlapping Periods Only", value = TRUE)
+        
+        ),
+            mainPanel(
+                fluidRow(
+                    column(6, plotOutput("laggedReturnsPlot")),
+                    column(6, plotOutput("laggedVolPlot"))
                 )
             )
         )
+    ),
+    
+    tabPanel(
+        "Backtest",
+        value = "backtestTab",
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput("initEqSlider", "Initial Equity, $", min = 1000, max = 100000, step = 1000, value = 10000),
+                fluidRow(
+                    column(6, 
+                        knobInput("commKnob", "Per Share Commission, cents", min = 0, max = 5, step = 0.05, value = 0.5, displayPrevious = TRUE)
+                    ),
+                    column(6, 
+                        knobInput("minCommKnob", "Minimum Commission Per Order, $", min = 0, max = 10, step = 0.5, value = 0.5, displayPrevious = TRUE)
+                    )
+                ),
+                fluidRow(
+                    column(6, 
+                        sliderInput("targetVolSlider", "Target Asset Volatility, %", min = 1, max = 10, step = 0.5, value = 5)
+                    ),
+                    column(6, 
+                        sliderInput("volLookbackSlider", "Volatility Estimation Window, days", min = 5, max = 120, step = 5, value = 60)
+                    )
+                ),
+                fluidRow(
+                    column(6, 
+                        sliderInput("rebalFreqSlider", "Rebalance Frequency, months", min = 1, max = 6, step = 1, value = 1)
+                    ),
+                    column(6, 
+                        sliderInput("capFreqSlider", "Frequency to Capitalise Profits", min = 0, max = 12, step = 1, value = 0)
+                    )
+                ),
+            ),
+            
+            mainPanel()
+        )
     )
     
+    # initEq <- 10000
+    # perShareComm <- 0.005
+    # minCommPerOrder <- 1 
+    # # TODO: Switch to calc commission on close not adjustedclose
+    # assetVolTarget <- 0.05
+    # volLookback <- 60
+    # 
+    # rebalFrequency <- 1 # rebalance frequency in months. Can't be zero otherwise will crap out.
+    # capFrequency <- 1 # frequence to capitalise profits. 0 = don't. 
 )
 
 server <- function(input, output) {
     
-    observe({
-        if(input$"tab" == "perfTab") {
-            shinyjs::disable(id = "estWdwSize")
-            shinyjs::disable(id = "fwdWdwSize")
-            shinyjs::disable(id = "removeOverlapping")
-        } else {
-            shinyjs::enable(id = "estWdwSize")
-            shinyjs::enable(id = "fwdWdwSize")
-            shinyjs::enable(id = "removeOverlapping")
-        }
-    })
+    # observe({
+    #     if(input$"tab" == "perfTab") {
+    #         shinyjs::disable(id = "estWdwSize")
+    #         shinyjs::disable(id = "fwdWdwSize")
+    #         shinyjs::disable(id = "removeOverlapping")
+    #     } else {
+    #         shinyjs::enable(id = "estWdwSize")
+    #         shinyjs::enable(id = "fwdWdwSize")
+    #         shinyjs::enable(id = "removeOverlapping")
+    #     }
+    # })
     
     df <- reactive({
         prices %>% 
