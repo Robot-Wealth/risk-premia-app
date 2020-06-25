79787fb459b29e7c9ae531a362df860dc5136c59
robotmaterkris <kris@robotwealth.com>
2020-06-25 19:12:21 +1000

    add backtest panels

---
+++ risk-premia-app/R/backtest_utils.R
@@ -0,0 +1,543 @@
+# backtest utilities
+
+# Setup backtest =======================
+
+get_monthends <- function(prices_df) {
+  prices_df %>% 
+      mutate(
+        year = year(date),
+        month = month(date)
+      ) %>%
+      group_by(year, month) %>%
+      summarise(date = max(date))
+}
+
+make_monthly_prices <- function(prices_df, monthends) {
+  prices_df %>%
+    inner_join(monthends, by = 'date') %>%
+    select(ticker, date, close = closeadjusted)
+  
+}
+
+monthends <- get_monthends(prices)
+
+monthlyprices <- make_monthly_prices(prices, monthends) %>% 
+  arrange(date) %>%
+  group_by(ticker) %>%
+  mutate(returns = ((close) / dplyr::lag(close)) - 1)
+
+startDate <- monthlyprices %>% summarise(min(date)) %>% pull()
+endDate <- monthlyprices %>% summarise(max(date)) %>% pull()
+initDate <- startDate - 1 # InitDate is day before startdate
+
+# Buy equal amount, don't rebalance =====
+
+num_shares <- function(monthlyprices_df, equity) {
+  monthlyprices_df %>%
+    filter(date == startDate) %>%
+    mutate(shares = trunc((equity / 3) / close)) %>%
+    select(ticker, shares)
+  
+}
+
+ew_norebal_positions <- function(monthlyprices_df, num_shares, perShareComm, minCommPerOrder) {
+  # calculate positions, exposures, trades, commissions
+  
+  monthlyprices_df %>%
+    inner_join(num_shares, by = 'ticker') %>%
+    mutate(exposure = shares * close) %>%
+    group_by(ticker) %>%
+    mutate(trades = shares - lag(shares)) %>%
+    # Initial trade to setup position comes through as NA
+    mutate(trades = case_when(is.na(trades) ~ shares, TRUE ~ trades)) %>%
+    mutate(tradevalue = trades * close) %>%
+    mutate(commission = case_when(abs(trades) * perShareComm > minCommPerOrder ~ abs(trades) * perShareComm, trades == 0 ~ 0, TRUE ~ minCommPerOrder))
+}
+
+get_init_cash_bal <- function(positions, inital_equity) {
+  positions %>% 
+    ungroup() %>%
+    filter(date == startDate) %>%
+    summarise(cash = inital_equity - sum(exposure) - sum(commission)) %>%
+    pull()
+}
+
+bind_cash_positions <- function(positions, initial_cash_balance, inital_equity) {
+  ticker_temp <- positions %>% distinct(ticker) %>% first() %>% pull()
+  
+  Cash <- positions %>%
+    ungroup() %>%
+    filter(ticker == ticker_temp) %>% # just doing this to get the dates
+    mutate(
+      ticker = 'Cash', 
+      date, 
+      close = 0, 
+      shares = 0, 
+      exposure = initial_cash_balance, 
+      trades = 0, 
+      tradevalue = case_when(date == startDate ~ initial_cash_balance - inital_equity, TRUE ~ 0), 
+      commission = 0
+    ) 
+  
+  positions %>%
+    # Bind cash balances and sort by date again
+    bind_rows(Cash) %>%
+    arrange(date)
+}
+
+stacked_area_chart <- function(positions, title) {
+  positions %>% 
+    ggplot(aes(x = date, y = exposure, fill = ticker)) +
+      geom_area() +
+      app_fill_scale +
+      labs(
+        x = "Date",
+        y = "Expsoure Value",
+        title = title
+      ) 
+}
+
+trades_chart <- function(positions, title) {
+  positions %>%
+    filter(ticker != 'Cash') %>% 
+    ggplot(aes(x = date, y = tradevalue, fill = ticker)) +
+      geom_bar(stat = 'identity', position = position_dodge()) +
+      app_fill_scale +
+      labs(
+        x = "Date",
+        y = "Trade Value",
+        title = title
+      )
+}
+
+comm_chart <- function(positions, title) {
+  # Trading cost as $
+  positions %>%
+    filter(ticker != 'Cash') %>% 
+    ggplot(aes(x=date, y=commission , fill = ticker)) +
+      geom_bar(stat = 'identity') +
+      app_fill_scale +
+      labs(
+        x = "Date",
+        y = "Commission",
+        title = title
+      )
+}
+
+comm_pct_exp_chart <- function(positions, title) {
+  # Trading cost as % of total exposure in instrument
+  positions %>%
+    filter(ticker != 'Cash') %>% 
+    mutate(commissionpct = commission / exposure) %>%
+    ggplot(aes(x = date, y = commissionpct , fill = ticker)) +
+      geom_bar(stat = 'identity', position = position_dodge()) +
+      app_fill_scale +
+      labs(
+        x = "Date",
+        y = "Commission",
+        title = title
+      )
+}
+
+# Performance metrics ===================
+
+calc_ann_turnover <- function(positions, mean_equity) {
+  # Calculate as total sell trades divided by mean equity * number of years
+  totalselltrades <- positions %>%
+    filter(
+      ticker != 'Cash',
+      tradevalue < 0,
+      date != startDate
+    ) %>%
+      summarise(sellvalue = sum(tradevalue)) %>%
+      pull() 
+  
+  if(length(totalselltrades) == 0) {
+    return(0)
+  } else {
+    -totalselltrades / (mean_equity * (year(endDate) - year(startDate)))
+  }
+}
+
+calc_port_returns <- function(positions) {
+  positions %>%
+    group_by(date) %>%
+    summarise(
+      totalequity = sum(exposure),
+      totalcommission = sum(commission)    
+    ) %>%
+    ungroup() %>%
+    arrange(date) %>%
+    mutate(returns = totalequity / lag(totalequity) - 1)
+}
+
+summary_performance <- function(positions, initial_equity) {
+  port_returns <- positions %>%
+    calc_port_returns() 
+  
+  mean_equity <- mean(port_returns$totalequity)
+  total_profit <- tail(port_returns, 1)$totalequity - initial_equity
+  costs_pct_profit <- sum(port_returns$totalcommission) / total_profit
+  
+  port_returns %>% 
+    tq_performance(Ra = returns, performance_fun = table.AnnualizedReturns) %>% 
+    bind_cols(c(
+      port_returns %>%
+        tq_performance(Ra = returns, performance_fun = table.DownsideRisk) %>%
+        select(MaximumDrawdown),
+      positions %>% 
+        calc_ann_turnover(mean_equity) %>% 
+        enframe(name = NULL, value = "Ave.Ann.Turnover"),
+      total_profit %>% 
+        enframe(name = NULL, value = "Tot.Profit"),
+      costs_pct_profit %>% 
+        enframe(name = NULL, value = "Costs(pct.Profit)")
+    ))
+}
+
+combine_port_asset_returns <- function(positions, returns_df) {
+  # df of port and asset returns
+  positions %>%
+    calc_port_returns() %>% 
+    select(date, returns) %>%
+    mutate(ticker = "Portfolio") %>% 
+    bind_rows(returns_df %>% select(ticker, date, returns)) %>% 
+    arrange(date)
+}
+
+rolling_ann_port_perf <- function(port_returns_df) {
+  port_returns_df %>% 
+    group_by(ticker) %>% 
+    arrange(date) %>% 
+    mutate(
+      roll_ann_return = sqrt(12)*roll_mean(returns, width = 12),
+      roll_ann_sd = sqrt(12)*roll_sd(returns, width = 12),
+      roll_sharpe = roll_ann_return/roll_ann_sd
+    ) %>% 
+    select(date, ticker, roll_ann_return, roll_ann_sd, roll_sharpe) %>% 
+    pivot_longer(cols = c(-ticker, -date), names_to = "metric", values_to = "value")
+}
+
+rolling_ann_port_perf_plot <- function(perf_df) {
+  metric_names <- as_labeller(c(
+    `roll_ann_return` = "Return",
+    `roll_ann_sd` = "Volatility",
+    `roll_sharpe` = "Sharpe"
+  ))
+  
+  perf_df %>% 
+    mutate(ticker = factor(ticker, levels = names(app_cols))) %>%
+    ggplot(aes(x = date, y = value, colour = ticker, size = ticker)) +
+      geom_line() +
+      app_col_scale +
+      scale_size_manual(
+        values = c(rep(0.6, length(rp_tickers)), 1.2),
+        labels = waiver()
+      ) +
+      facet_wrap(~metric, scales = "free_y", ncol = 1, labeller = metric_names) +
+      labs(
+        x = "Date",
+        y = "Value",
+        title = "Rolling Annualised Performance Statistics"
+      ) 
+}
+
+# TODO:
+# - refactor
+# - replace for loop with map_df
+share_based_backtest <- function(monthlyprices_df, initial_equity, cap_frequency, rebal_frequency, per_share_Comm, min_comm, rebal_method = "ew") {
+  
+  stopifnot(rebal_method %in% c("ew", "rp"))
+  
+  # Create wide data frames for simple share based backtest
+  wideprices <- monthlyprices_df %>%
+    pivot_wider(date, names_from = 'ticker', values_from = 'close')
+  
+  if(rebal_method == "rp") {
+    widetheosize <- monthlyprices_df %>%
+      pivot_wider(date, names_from = 'ticker', values_from = 'theosize_constrained')
+  }
+  
+  rowlist <- list()
+  
+  Cash <- initial_equity
+  sharepos <- c(0,0,0)
+  sharevalue <- c(0,0,0)
+  equity <- initial_equity
+  capEquity <- initial_equity # Sticky equity amount that we're using to allow change cap frequency
+  
+  # Iterate through prices and backtest
+  for (i in 1:(nrow(wideprices))) {
+    currentdate <- wideprices[i,1] %>% pull() %>% as.Date()
+    currentprice <- as.numeric(wideprices[i,2:4])
+    if(rebal_method == "rp") {
+      currenttheosize <- as.numeric(widetheosize[i, 2:4])
+    }
+    equity <- sum(sharepos * currentprice) + Cash
+    
+    # Update capEquity if it's re-capitalisation rebalance time
+    if(cap_frequency > 0) {
+      if(i %% cap_frequency == 0) capEquity <- equity 
+    }
+    
+    # Update position sizing if its position rebalance time 
+    if (i == 1 | i %% rebal_frequency == 0) {
+      if(rebal_method == "ew") {
+        targetshares <- trunc((capEquity / 3) / currentprice) 
+      } else {
+        targetshares <- trunc((capEquity * currenttheosize) / currentprice)  
+      }
+    }
+    
+    trades <- targetshares - sharepos
+    tradevalue <- trades * currentprice
+    commissions <- abs(trades) * per_share_Comm
+    commissions[commissions < min_comm] <- min_comm
+    
+    # Adjust cash by value of trades
+    Cash <- Cash - sum(tradevalue) - sum(commissions)
+    sharepos <- targetshares
+    sharevalue <- sharepos * currentprice
+    equity <- sum(sharevalue) + Cash
+    
+    # Create data frame and add to list
+    row_df <- data.frame(
+       ticker = c('Cash', colnames(wideprices[2:4])),
+       date = rep(currentdate, 4),
+       close = c(0,currentprice),
+       shares = c(0,sharepos),
+       exposure = c(Cash, sharevalue),
+       sharetrades = c(0, trades),
+       tradevalue = c(-sum(tradevalue), tradevalue),
+       commission = c(0, commissions)
+    )
+    
+    rowlist[[i]] <- row_df
+    
+  }
+  
+  # Combine list into dataframe
+  bind_rows(rowlist)
+  
+}
+
+# ew <- share_based_backtest(monthlyprices, 10000, 0.0005, 0.5)
+# summary_performance(ew, 10000)
+# 
+# rp <- share_based_backtest(volsizeprices, 10000, 0.0005, 0.5, rebal_method = "rp")
+# summary_performance(rp, 10000)
+
+# Vol targeting functions ===============
+
+# Calculate vol target sizing on daily data
+calc_vol_target <- function(prices_df, vol_lookback, target_vol) {
+  prices %>%
+    group_by(ticker) %>%
+    arrange(date) %>%
+    mutate(returns = (closeadjusted / dplyr::lag(closeadjusted)) - 1) %>%
+    mutate(vol = slider::slide_dbl(.x = returns, .f = sd, .before = vol_lookback, .complete = TRUE) * sqrt(252)) %>%
+    mutate(theosize = lag(target_vol / vol))
+}
+
+# Enforce leverage constraint
+cap_leverage <- function(vol_targets, max_leverage = 1) {
+  total_size <- vol_targets %>%
+    group_by(date) %>%
+    summarise(totalsize = sum(theosize)) %>%
+    mutate(adjfactor = case_when(totalsize > max_leverage ~ max_leverage/totalsize, TRUE ~ 1))
+  
+  vol_targets %>%
+    inner_join(total_size, by = 'date') %>%
+    mutate(theosize_constrained = theosize * adjfactor) %>%
+    select(ticker, date, closeadjusted, returns, vol, theosize, theosize_constrained) %>%
+    na.omit()
+}
+
+# Plot theoretical constrained sizing as a function of time
+constrained_sizing_plot <- function(volsized_prices, title) {
+  volsized_prices %>%
+    ggplot(aes(x = date, y = theosize_constrained, fill = ticker)) +
+    geom_area() + 
+    app_fill_scale +
+    labs(
+      x = "Date",
+      y = "Constrained Sizing", 
+      title = title
+    )
+}
+
+# EW backtest reactives =================
+
+ew_norebal <- reactive({
+  shares <- num_shares(monthlyprices, input$initEqSlider)
+  
+  pos <- monthlyprices %>% 
+    ew_norebal_positions(shares, input$commKnob/100., as.double(input$minCommKnob))
+  
+  initcashbal <- pos %>%
+    get_init_cash_bal(input$initEqSlider)
+  
+  pos <- pos %>% 
+    bind_cash_positions(initcashbal, input$initEqSlider)
+  
+})
+
+output$ewbhEqPlot <- renderPlot({
+  ew_norebal() %>% 
+    stacked_area_chart('3 ETF USD Risk Premia - Equal Weight, No Rebalancing')
+})
+
+output$ewbhRollPerfPlot <- renderPlot({
+  ew_norebal() %>% 
+    combine_port_asset_returns(monthlyprices) %>% 
+    rolling_ann_port_perf() %>% 
+    rolling_ann_port_perf_plot()
+})
+
+output$ewbhTradesPlot <- renderPlot({
+  ew_norebal() %>% 
+    trades_chart('3 ETF USD Risk Premia - Trades')
+})
+
+output$ewbhCommPlot <- renderPlot({
+  ew_norebal() %>% 
+    comm_chart('3 ETF USD Risk Premia -  Commission ($)')
+})
+
+output$ewbhCommExpPlot <- renderPlot({
+  ew_norebal() %>% 
+    comm_pct_exp_chart('3 ETF USD Risk Premia -  Commission as pct of exposure')
+})
+
+output$ewbhPerfTable <- renderTable({
+  ew_norebal() %>% 
+    summary_performance(input$initEqSlider)
+})
+
+output$ewbhTradesTable <- renderDataTable({
+  ew_norebal()
+})
+
+# EW rebalanced backtest reactives =================
+
+ew_rebal <- reactive({
+  share_based_backtest(monthlyprices, input$initEqSlider, input$capFreqSlider, input$rebalFreqSlider,  input$commKnob/100., as.double(input$minCommKnob))
+})
+
+output$ewrebEqPlot <- renderPlot({
+  ew_rebal() %>% 
+    stacked_area_chart('3 ETF USD Risk Premia - Equal Weight, Rebalancing')
+})
+
+output$ewrebRollPerfPlot <- renderPlot({
+  ew_rebal() %>% 
+    combine_port_asset_returns(monthlyprices) %>% 
+    rolling_ann_port_perf() %>% 
+    rolling_ann_port_perf_plot()
+})
+
+output$ewrebTradesPlot <- renderPlot({
+  ew_rebal() %>% 
+    trades_chart('3 ETF USD Risk Premia - Trades')
+})
+
+output$ewrebCommPlot <- renderPlot({
+  ew_rebal() %>% 
+    comm_chart('3 ETF USD Risk Premia -  Commission ($)')
+})
+
+output$ewrebCommExpPlot <- renderPlot({
+  ew_rebal() %>% 
+    comm_pct_exp_chart('3 ETF USD Risk Premia -  Commission as pct of exposure')
+})
+
+# TODO: figure out why performance output is repeated in shiny output
+output$ewrebPerfTable <- renderTable({
+  ew_rebal() %>% 
+    summary_performance(input$initEqSlider) %>% 
+    slice(1)
+})
+
+output$ewrebTradesTable <- renderDataTable({
+  ew_rebal()
+})
+
+
+# RP backtest reactives =================
+
+volsize_prices <- reactive({
+  theosize_constrained <- calc_vol_target(prices, input$volLookbackSlider, input$targetVolSlider/100.) %>% 
+    cap_leverage()
+  
+  # Get the snapshots at the month end boundaries
+  monthlyprices %>%
+    inner_join(select(theosize_constrained, ticker, date, theosize_constrained), by = c('ticker','date'))
+})
+
+output$rpTheoSizePlot <- renderPlot({
+  volsize_prices() %>% 
+    constrained_sizing_plot(title = '3 ETF USD Risk Premia - Theoretical Constrained Sizing (% of Portfolio Equity')
+})
+
+
+rp_rebal <- reactive({
+  share_based_backtest(volsize_prices(), input$initEqSlider, input$capFreqSlider, input$rebalFreqSlider, input$commKnob/100., as.double(input$minCommKnob), rebal_method = "rp")
+})
+
+output$rpEqPlot <- renderPlot({
+  rp_rebal() %>% 
+    stacked_area_chart('3 ETF USD Risk Premia - Simple Risk Parity')
+})
+
+output$rpRollPerfPlot <- renderPlot({
+  rp_rebal() %>% 
+    combine_port_asset_returns(monthlyprices) %>% 
+    rolling_ann_port_perf() %>% 
+    rolling_ann_port_perf_plot()
+})
+
+output$rpTradesPlot <- renderPlot({
+  rp_rebal() %>% 
+    trades_chart('3 ETF USD Risk Premia - Trades')
+})
+
+output$rpCommPlot <- renderPlot({
+  rp_rebal() %>% 
+    comm_chart('3 ETF USD Risk Premia - Commission ($)')
+})
+
+output$rpCommExpPlot <- renderPlot({
+  rp_rebal() %>% 
+    comm_pct_exp_chart('3 ETF USD Risk Premia -  Commission as pct of exposure')
+})
+
+# TODO: figure out why performance output is repeated in shiny output
+output$rpPerfTable <- renderTable({
+  rp_rebal() %>% 
+    summary_performance(input$initEqSlider) %>% 
+    slice(1)
+})
+
+output$ewrebTradesTable <- renderDataTable({
+  rp_rebal()
+})
+
+
+# rp <- share_based_backtest(volsizeprices, 10000, 0.0005, 0.5, rebal_method = "rp")
+# summary_performance(rp, 10000)
+# theosize_constrained <- calc_vol_target(prices, 60, 5/100.) %>%
+#   cap_leverage()
+# 
+# volsize_prices<- monthlyprices %>%
+#   inner_join(select(theosize_constrained, ticker, date, theosize_constrained), by = c('ticker','date'))
+# 
+# volsize_prices %>% 
+#   constrained_sizing_plot(title = '3 ETF USD Risk Premia - Theoretical Constrained Sizing (% of Portfolio Equity')
+# 
+# rp_rebal <- share_based_backtest(volsize_prices, 10000, 1, 1, 0.5/100., 0.5, rebal_method = "rp")
+# 
+# rp_rebal %>% 
+#   stacked_area_chart('3 ETF USD Risk Premia - Simple Risk Parity')
+
+
+
--- risk-premia-app/app.R
+++ risk-premia-app/app.R
@@ -4,38 +4,52 @@ library(here)
 library(shinyjs)
 library(shinyWidgets)
 library(DT)
+library(lubridate)
+library(tidyquant)
+library(slider)
+#library(shinycssloaders)
 
 theme_set(theme_bw())
+
+gg_color_hue <- function(n) {
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+app_cols <- gg_color_hue(5)
+rp_tickers <- c("VTI", "TLT", 'GLD')
+names(app_cols) <- c(rp_tickers, "Cash", "Portfolio")
+app_col_scale <- scale_colour_manual(name = "ticker", values = app_cols)
+app_fill_scale <- scale_fill_manual(name = "ticker", values = app_cols)
 
 # load data, functions
 
 # to run locally
-load(here::here("risk-premia-app", "data", "assetclass_prices.RData"))  
-source(here::here("risk-premia-app", "R", "utils.R"), local = TRUE) 
+# load(here::here("risk-premia-app", "data", "assetclass_prices.RData"))  
+# source(here::here("risk-premia-app", "R", "utils.R"), local = TRUE) 
 
 # for deployment
-# source("R/utils.R", local = TRUE)
-# load("data/assetclass_prices.RData", local = TRUE)  
+source("R/utils.R", local = TRUE)
+load("data/assetclass_prices.RData")
 # source("R/backtest_utils.R", local = TRUE) 
 
 # TODO:
-    # load from BQ: do we want the unwashed masses hitting our cloud?
     # maybe load data from BQ on the first startup of the day so it's always up to date without always hitting BQ
     # needs application service token
-    # Apply colour scheme such that colours of already selected assets don't change - 
-    # rolling correlation: implement with roll not slider
+    # Apply colour scheme such that colours of already selected assets don't change: DONE
+    # rolling correlation: implement with roll not slider: DONE
     # spinners on plot load
     # tour
     # rolling corrplot - should use different colours as they don't represent the things above....
     # numbers over tiles on corrplot
     # tooltip for freq to capitalise profits
-    # disable irrelevant inputs on backesting tabs
     # setup backtest params could go into global_utils (same for all sessions....)
+    # performance tables to three decimal places
+    # disable inputs on backtest tabs: DONE
+
 
 
 # calculate total returns
-rp_tickers <- c("VTI", "TLT", 'GLD')
-
 prices <- prices %>% 
     filter(ticker %in% rp_tickers) %>% 
     add_total_returns_col()
@@ -90,10 +104,10 @@ ui <- navbarPage(
         value = "backtestTab",
         sidebarLayout(
             sidebarPanel(
-                sliderInput("initEqSlider", "Initial Equity, $", min = 1000, max = 100000, step = 1000, value = 10000),
+                sliderInput("initEqSlider", "Initial Equity, $", min = 1000, max = 1000000, step = 1000, value = 10000),
                 fluidRow(
                     column(6, 
-                        knobInput("commKnob", "Per Share Commission, cents", min = 0, max = 5, step = 0.05, value = 0.5, displayPrevious = TRUE)
+                        knobInput("commKnob", "Per Share Commission, cents", min = 0.1, max = 2, step = 0.01, value = 0.5, displayPrevious = TRUE)
                     ),
                     column(6, 
                         knobInput("minCommKnob", "Minimum Commission Per Order, $", min = 0, max = 10, step = 0.5, value = 0.5, displayPrevious = TRUE)
@@ -109,22 +123,25 @@ ui <- navbarPage(
                 ),
                 fluidRow(
                     column(6, 
-                        sliderInput("rebalFreqSlider", "Rebalance Frequency, months", min = 1, max = 6, step = 1, value = 1)
+                        sliderInput("rebalFreqSlider", "Rebalance Frequency, months", min = 1, max = 12, step = 1, value = 1)
                     ),
                     column(6, 
-                        sliderInput("capFreqSlider", "Frequency to Capitalise Profits", min = 0, max = 12, step = 1, value = 0)
+                        sliderInput("capFreqSlider", "Frequency to Capitalise Profits", min = 0, max = 12, step = 1, value = 1)
                     )
                 ),
             ),
             
             mainPanel(
                 tabsetPanel(
+                    id = "backtestPanel",
                     tabPanel(
                         "Equal Weight Buy and Hold",
+                        value = "ewbhTab",
                         fluidRow(
                             column(12, align = "center", 
                                 plotOutput("ewbhEqPlot"),
                                 tableOutput("ewbhPerfTable"),
+                                plotOutput("ewbhRollPerfPlot"),
                                 plotOutput("ewbhTradesPlot", height = "150px"),
                                 plotOutput("ewbhCommPlot", height = "150px"),
                                 plotOutput("ewbhCommExpPlot", height = "150px"),
@@ -133,7 +150,34 @@ ui <- navbarPage(
                         )
                     ),
                     tabPanel(
-                    "Next Backtest"
+                    "Equal Weight Rebalance",
+                    value = "ewrebalTab", 
+                        fluidRow(
+                            column(12, align = "center", 
+                               plotOutput("ewrebEqPlot"),
+                               tableOutput("ewrebPerfTable"),
+                               plotOutput("ewrebRollPerfPlot"),
+                               plotOutput("ewrebTradesPlot", height = "150px"),
+                               plotOutput("ewrebCommPlot", height = "150px"),
+                               plotOutput("ewrebCommExpPlot", height = "150px"),
+                               DT::dataTableOutput(("ewrebTradesTable"))
+                            )
+                        )
+                    ),
+                    tabPanel(
+                        "Risk Parity",
+                        fluidRow(
+                            column(12, align = "center", 
+                               plotOutput("rpEqPlot"),
+                               plotOutput("rpTheoSizePlot", height = "150px"),
+                               tableOutput("rpPerfTable"),
+                               plotOutput("rpRollPerfPlot"),
+                               plotOutput("rpTradesPlot", height = "150px"),
+                               plotOutput("rpCommPlot", height = "150px"),
+                               plotOutput("rpCommExpPlot", height = "150px"),
+                               DT::dataTableOutput(("rpTradesTable"))
+                            )
+                        )
                     )
                 )
             )
@@ -153,20 +197,28 @@ ui <- navbarPage(
 
 server <- function(input, output) {
     
-    source(here::here("risk-premia-app", "R", "backtest_utils.R"), local = TRUE) 
-    # source("R/backtest_utils.R", local = TRUE) 
+    # source(here::here("risk-premia-app", "R", "backtest_utils.R"), local = TRUE) 
+    source("R/backtest_utils.R", local = TRUE) 
     
-    # observe({
-    #     if(input$"tab" == "perfTab") {
-    #         shinyjs::disable(id = "estWdwSize")
-    #         shinyjs::disable(id = "fwdWdwSize")
-    #         shinyjs::disable(id = "removeOverlapping")
-    #     } else {
-    #         shinyjs::enable(id = "estWdwSize")
-    #         shinyjs::enable(id = "fwdWdwSize")
-    #         shinyjs::enable(id = "removeOverlapping")
-    #     }
-    # })
+    observe({
+        if(input$backtestPanel == "ewbhTab") {
+            shinyjs::disable(id = "volLookbackSlider")
+            shinyjs::disable(id = "targetVolSlider")
+            shinyjs::disable(id = "rebalFreqSlider")
+            shinyjs::disable(id = "capFreqSlider")
+            
+        } else if (input$backtestPanel == "ewrebalTab") {
+            shinyjs::disable(id = "volLookbackSlider")
+            shinyjs::disable(id = "targetVolSlider")
+            shinyjs::enable(id = "rebalFreqSlider")
+            shinyjs::enable(id = "capFreqSlider")
+        } else {
+            shinyjs::enable(id = "rebalFreqSlider")
+            shinyjs::enable(id = "capFreqSlider")
+            shinyjs::enable(id = "volLookbackSlider")
+            shinyjs::enable(id = "targetVolSlider")
+        }
+    })
     
     df <- reactive({
         prices %>% 
